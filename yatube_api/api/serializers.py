0111c233b4b82b2b5d3d9a257359c0b9cdacf100
Roman Garbuz <roma.garbuz12@yandex.ru>
2024-01-16 12:52:21 +0300

    Sprint12 project v2

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,18 +1,10 @@
-import base64
-
-from django.core.files.base import ContentFile
-
-from posts.models import Comment, Follow, Group, Post, User
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
 
+from posts.models import Comment, Follow, Group, Post
 
-class Base64ImageField(serializers.ImageField):
-    def to_internal_value(self, data):
-        if isinstance(data, str) and data.startswith('data:image'):
-            format, imgstr = data.split(';base64,')
-            ext = format.split('/')[-1]
-            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
-        return super().to_internal_value(data)
+User = get_user_model()
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -21,7 +13,6 @@ class PostSerializer(serializers.ModelSerializer):
         default=serializers.CurrentUserDefault(),
         slug_field='username'
     )
-    image = Base64ImageField(required=False, allow_null=True)
 
     class Meta:
         fields = ('id', 'text', 'author', 'image', 'group', 'pub_date')
@@ -29,14 +20,16 @@ class PostSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField(
-        default=serializers.CurrentUserDefault()
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        default=serializers.CurrentUserDefault(),
+        slug_field='username'
     )
 
     class Meta:
         fields = ('id', 'author', 'post', 'text', 'created')
         model = Comment
-        read_only_fields = ['author', 'post']
+        read_only_fields = ['post']
 
 
 class GroupSerializer(serializers.ModelSerializer):
@@ -59,3 +52,15 @@ class FollowSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('id', 'user', 'following')
         model = Follow
+        validators = [
+            UniqueTogetherValidator(
+                queryset=User.objects.all(),
+                fields=['user', 'following'],
+                message='Повторная подписка невозможна!'
+            )
+        ]
+
+    def validate(self, data):
+        if self.context['request'].user == data['following']:
+            raise serializers.ValidationError('Подписка на себя невозможна!')
+        return data
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,53 +1,35 @@
-from django.core.exceptions import BadRequest, PermissionDenied
+from django.core.exceptions import BadRequest
 from django.shortcuts import get_object_or_404
-
-from posts.models import Group, Post
 from rest_framework import filters, permissions, viewsets
 from rest_framework.pagination import LimitOffsetPagination
 
+from posts.models import Group, Post
+
+from .permissions import IsAuthorOrReadOnly
 from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
                           PostSerializer)
+from .utils import CreateListViewSet
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     pagination_class = LimitOffsetPagination
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsAuthorOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-    def perform_update(self, serializer):
-        if serializer.instance.author != self.request.user:
-            raise PermissionDenied('Изменение чужих постов запрещено!')
-        super(PostViewSet, self).perform_update(serializer)
-
-    def perform_destroy(self, serializer):
-        if serializer.author != self.request.user:
-            raise PermissionDenied('Удаление чужих постов запрещено!')
-        super(PostViewSet, self).perform_destroy(serializer)
-
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsAuthorOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(
             author=self.request.user,
             post=get_object_or_404(Post, id=self.kwargs.get('post_id'))
         )
-
-    def perform_update(self, serializer):
-        if serializer.instance.author != self.request.user:
-            raise PermissionDenied('Изменение чужих комментариев запрещено!')
-        super(CommentViewSet, self).perform_update(serializer)
-
-    def perform_destroy(self, serializer):
-        if serializer.author != self.request.user:
-            raise PermissionDenied('Удаление чужих комментариев запрещено!')
-        super(CommentViewSet, self).perform_destroy(serializer)
 
     def get_queryset(self):
         post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
@@ -60,27 +42,17 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 
-class FollowViewSet(viewsets.ModelViewSet):
+class FollowViewSet(CreateListViewSet):
     serializer_class = FollowSerializer
     filter_backends = (filters.SearchFilter,)
-    search_fields = ('following__username',)
+    search_fields = ('=following__username',)
 
     def perform_create(self, serializer):
         following = serializer.validated_data['following']
         user = self.request.user
-        if following == user:
-            raise BadRequest('Подписаться на себя нельзя!')
         if user.follows.filter(following=following):
             raise BadRequest('Вы уже подписаны на этого пользователя!')
         serializer.save(user=user)
-
-    def perform_update(self, serializer):
-        raise BadRequest('Изменить подписку нельзя!')
-
-    def perform_destroy(self, serializer):
-        if serializer.user != self.request.user:
-            raise PermissionDenied('Удаление чужих подписок запрещено!')
-        super(FollowViewSet, self).perform_destroy(serializer)
 
     def get_queryset(self):
         user = self.request.user
